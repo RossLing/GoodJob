53073beb44f7920a8b7a71a45421157dcdd2f4b9
April.Zhang <linlin0055@163.com>
2019-02-25 14:28:51 +0800

    build

---
+++ build/precache-manifest.6e2d3a6a0a217afe1de8a6b70ecdf387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1734143c60eb98bb1e62bf071ec81fce",
+    "url": "/static/media/FotoJet.1734143c.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "24fc9a45acab55e823fd",
+    "url": "/static/js/main.24fc9a45.chunk.js"
+  },
+  {
+    "revision": "37f54bfb85982f36bc64",
+    "url": "/static/js/2.37f54bfb.chunk.js"
+  },
+  {
+    "revision": "24fc9a45acab55e823fd",
+    "url": "/static/css/main.f83ce114.chunk.css"
+  },
+  {
+    "revision": "37f54bfb85982f36bc64",
+    "url": "/static/css/2.c3f24d78.chunk.css"
+  },
+  {
+    "revision": "52df24159f23d656105386fbef3943a4",
+    "url": "/index.html"
+  }
+];
